1019155802b05225aa72a41e4e7d1dfbdb409428
ninedraft <clamores.pro@gmail.com>
2019-03-01 16:30:51 +0300

    add main cli command call

--- cmd/flex/main.go
+++ cmd/flex/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/ninedraft/flex/pkg/cli"
+)
+
+func main() {
+	if err := cli.Main().Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
