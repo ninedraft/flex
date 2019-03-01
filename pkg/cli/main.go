ec936f16fda5a61e1623f69a47884ce305869702
ninedraft <clamores.pro@gmail.com>
2019-03-01 15:25:38 +0300

    add cli skeleton

---
+++ pkg/cli/main.go
@@ -0,0 +1,25 @@
+package cli
+
+import (
+	gpflag "github.com/octago/sflags/gen/gpflag"
+	cobra "github.com/spf13/cobra"
+)
+
+// flag definitions here
+// https://github.com/octago/sflags#flags-based-on-structures------
+type MainConfig struct {
+}
+
+func Main() *cobra.Command {
+	var flags MainConfig
+	var cmd = &cobra.Command{
+		Use: "command",
+		Run: func(cmd *cobra.Command, args []string) {
+
+		},
+	}
+	if err := gpflag.ParseTo(&flags, cmd.PersistentFlags()); err != nil {
+		panic(err)
+	}
+	return cmd
+}
