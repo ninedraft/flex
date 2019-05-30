56f3c7879142d3417171384d97d916c41d76a710
ninedraft <clamores.pro@gmail.com>
2019-05-30 12:46:18 +0300

    adds generaor model

--- pkg/generator/generator.go
+++ pkg/generator/generator.go
@@ -1,75 +1,54 @@
 package generator
 
 import (
-	"fmt"
-	"go/ast"
-	"go/parser"
-	"go/token"
-	"os"
-	"path/filepath"
+	"io"
+	"strings"
+
+	. "github.com/dave/jennifer/jen"
 )
 
 type Generator struct {
-	ast     ast.Node
-	structs map[string]StructReflex
+	Spec                Spec
+	PointerReciever     bool
+	MethodPrefix        string
+	ForcePrivateMethods bool
 }
 
-func GeneratorFromAST(AST ast.Node) *Generator {
-	return &Generator{
-		ast:     AST,
-		structs: extractStructsFromAST(AST),
+func (gen Generator) MethodGenerators() map[string]func() *Statement {
+	return map[string]func() *Statement{
+		"GetFieldNames": gen.GenerateGetFieldNames,
+		"FieldsN":       gen.GenerateFieldsN,
+		"GetField":      gen.GenerateGetField,
 	}
 }
 
-func ParseFile(fileContent string) (*Generator, error) {
-	var fileSet = token.NewFileSet()
-	var AST, errParseFile = parser.ParseFile(fileSet, "file.go",
-		fileContent, parser.ParseComments)
-	if errParseFile != nil {
-		return nil, errParseFile
-	}
-	return GeneratorFromAST(AST), nil
-}
-
-func ParsePackageDir(dirPath, packageName string) (*Generator, error) {
-	var fileSet = token.NewFileSet()
-	var fileFilter = func(fileInfo os.FileInfo) bool {
-		var ext = filepath.Ext(fileInfo.Name())
-		return ext == ".go"
-	}
-	var packages, errParseDir = parser.ParseDir(fileSet, dirPath,
-		fileFilter, parser.ParseComments)
-	if errParseDir != nil {
-		return nil, errParseDir
-	}
-	var pkg, pkgExists = packages[packageName]
-	if !pkgExists {
-		return nil, fmt.Errorf("generator: package %q doesn't exist in dir %q", packageName, dirPath)
-	}
-	return GeneratorFromAST(pkg), nil
-}
-
-func extractStructsFromAST(AST ast.Node) map[string]StructReflex {
-	var structs = make(map[string]StructReflex)
-	ast.Inspect(AST, func(node ast.Node) bool {
-		var typeSpec, isTypeSpec = node.(*ast.TypeSpec)
-		if !isTypeSpec {
-			return true
+func (gen Generator) Render(wr io.Writer, pkg string, generators ...func() *Statement) error {
+	if len(generators) == 0 {
+		for _, generator := range gen.MethodGenerators() {
+			generators = append(generators, generator)
 		}
-		var _, isStructSpec = typeSpec.Type.(*ast.StructType)
-		if !isStructSpec {
-			return true
-		}
-		structs[typeSpec.Name.Name] = StructReflexFromAST(typeSpec)
-		return true
-	})
-	return structs
+	}
+	var file = NewFile(pkg)
+	for _, methodGen := range generators {
+		file.Add(methodGen().Line())
+	}
+	return file.Render(wr)
 }
 
-func commentsFromAST(commentList []*ast.Comment) []string {
-	var comments = make([]string, 0, len(commentList))
-	for _, comment := range commentList {
-		comments = append(comments, comment.Text)
+func (gen Generator) recvArgs() *Statement {
+	if gen.PointerReciever {
+		return gen.recv().Op("*").Id(gen.Spec.Name)
 	}
-	return comments
+	return gen.recv().Id(gen.Spec.Name)
+}
+
+func (gen Generator) recv() *Statement {
+	return Id("_" + gen.Spec.Name)
+}
+
+func (gen Generator) methodName(name string) string {
+	if !gen.ForcePrivateMethods {
+		name = strings.Title(name)
+	}
+	return gen.MethodPrefix + name
 }
